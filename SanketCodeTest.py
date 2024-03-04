dc0fa1f2459d04dcb7dbf2fe43163b91e115ff4c
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-04 13:07:27 +0000

    Update SanketCodeTest.py

--- SanketCodeTest.py
+++ SanketCodeTest.py
@@ -31,10 +31,10 @@ class TestDataLoading(unittest.TestCase):
         # Perform data loading to Hive
         hive_database_name = "sanket_db"
         hive_table_name = "health_insurance"
-        df_postgres.write.mode('overwrite').saveAsTable(f"{hive_database_name}.{hive_table_name}")
+        df_postgres.write.mode('overwrite').saveAsTable(f"{sanket_db}.{health_insurance}")
 
         # Read Hive table
-        df_hive = self.spark.read.table(f"{hive_database_name}.{hive_table_name}")
+        df_hive = self.spark.read.table(f"{sanket_db}.{health_insurance}")
 
         # Verify the number of rows loaded to Hive
         actual_count = df_hive.count()
