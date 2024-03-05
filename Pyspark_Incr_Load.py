fac593f9faac6885c99edbae3679e4e507fda30f
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-05 16:48:21 +0000

    Update Pyspark_Incr_Load.py

--- Pyspark_Incr_Load.py
+++ Pyspark_Incr_Load.py
@@ -31,7 +31,12 @@ class TestIncrDataLoading(unittest.TestCase):
         postgres_table_name = "health_insurance"
         where_condition = "BeneID = 'BENE177334'"
         # df_postgres = self.spark.read.jdbc(url=postgres_url, table=postgres_table_name, properties=postgres_properties)
-        df_postgres = self.spark.read.jdbc(url=postgres_url, table=postgres_table_name, properties=postgres_properties, column=where_condition)
+        df_postgres = self.spark.read.jdbc(url=postgres_url, table=postgres_table_name, properties=postgres_properties,
+                                    column=None,  # Specify None for column
+                                    lowerBound=None,  # Specify None for lowerBound
+                                    upperBound=None,  # Specify None for upperBound
+                                    numPartitions=None,  # Specify None for numPartitions
+                                    predicates=[where_condition])  # Use predicates parameter for WHERE condition
 
         # Perform data loading to Hive
         df_postgres.write.mode('overwrite').saveAsTable("sanket_db.health_insurance")
