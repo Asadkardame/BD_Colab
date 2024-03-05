e85ab10bda8e690eb3c8c1e332d69eb8f84e7944
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-05 19:09:11 +0000

    Update Pyspark_Incr_Load.py

--- Pyspark_Incr_Load.py
+++ Pyspark_Incr_Load.py
@@ -18,7 +18,7 @@ class TestIncrDataLoading(unittest.TestCase):
         # Read initial count of rows from Hive table
         hive_database_name = "sanket_db"
         hive_table_name = "health_insurance"
-        initial_count_df = self.spark.sql(f"SELECT COUNT(*) AS count FROM {hive_database_name}.{hive_table_name}")
+        initial_count_df = self.spark.sql("SELECT COUNT(*) AS count FROM sanket_db.health_insurance")
         initial_count = initial_count_df.collect()[0]["count"]
 
         try:
