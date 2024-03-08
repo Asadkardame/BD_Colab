72436573d5a7be3b4792c74b8c451f95fecdd78f
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-08 17:09:25 +0000

    Update Pyspark_Incr_Load.py

--- Pyspark_Incr_Load.py
+++ Pyspark_Incr_Load.py
@@ -18,6 +18,7 @@ def test_incremental_load(self):
     # Define the query with the WHERE condition
     # query = f"(SELECT * FROM car_insurance_claims1 WHERE {whereCondition}) AS data"
     query = "(SELECT * FROM car_insurance_claims1 WHERE " + whereCondition + ") AS data"
+    print("Generated SQL query:", query)
 
 
     # Read new data from PostgreSQL with the WHERE condition
