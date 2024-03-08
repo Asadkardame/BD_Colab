5e758394fd9b2244bd826845c3bc6a30a5324b84
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-08 17:57:54 +0000

    Update Data_Validation.py

--- Data_Validation.py
+++ Data_Validation.py
@@ -28,17 +28,17 @@ print("Data Types:", data_types)
 
 # Data Validation Checks
 # Check for missing values in specific columns
-columns_to_check_null = ['POLICY_NUMBER', 'AGE', 'BIRTH']
+columns_to_check_null = ['NAME', 'AGE', 'BIRTH']
 missing_values_specific_columns = df.select([count(when(isnull(c), c)).alias(c) for c in columns_to_check_null]).collect()[0]
 print("Missing Values in Specific Columns:", missing_values_specific_columns)
 
 # Check for data types in specific columns
-columns_to_check_data_type = {'POLICY_NUMBER': 'integer', 'NAME': 'string'}
+columns_to_check_data_type = {'AGE': 'integer', 'NAME': 'string'}
 incorrect_data_types = [(col_name, actual_type) for col_name, actual_type in df.dtypes if col_name in columns_to_check_data_type and actual_type != columns_to_check_data_type[col_name]]
 print("Incorrect Data Types in Specific Columns:", incorrect_data_types)
 
 # Check for unique values in specific columns
-columns_to_check_uniqueness = ['POLICY_NUMBER', 'AGE']
+columns_to_check_uniqueness = ['INCOME', 'AGE']
 unique_values_specific_columns = {col_name: df.select(col_name).distinct().count() for col_name in columns_to_check_uniqueness}
 print("Unique Values in Specific Columns:", unique_values_specific_columns)
 
