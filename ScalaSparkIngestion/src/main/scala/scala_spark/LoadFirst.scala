d703d222af23067bc904e76bbd649ea73b0fef8d
ellie-seyedin <elahehseyedain93@gmail.com>
2024-03-11 14:19:12 +0000

    people table is updated

--- ScalaSparkIngestion/src/main/scala/scala_spark/LoadFirst.scala
+++ ScalaSparkIngestion/src/main/scala/scala_spark/LoadFirst.scala
@@ -7,7 +7,7 @@ object LoadFirst {
 
   def main(args: Array[String]): Unit = {
     val spark: SparkSession = SparkSession.builder().master("local[*]").appName("MiniPrjScala").enableHiveSupport().getOrCreate()
-    val df = spark.read.format("jdbc").option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb").option("dbtable", "people").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "welcomeitc@2022").load()
+    val df = spark.read.format("jdbc").option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb").option("dbtable", "people").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").load()
     println(df.printSchema())
     println(df.show(10))
     println("Automated")
@@ -18,6 +18,6 @@ object LoadFirst {
     df.show(10)
 
 
-    df.write.mode( "overwrite").saveAsTable("usukprjdb.testscalaspark")
+    df.write.mode( "overwrite").saveAsTable("usukprjdb.people")
     println("In Hive")
-  }
+  }}
