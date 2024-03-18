0ec94cdca5a8a3a28b85807715cac0eee47f1557
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-18 18:47:47 +0000

    Update Data_Validation.py

--- Data_Validation.py
+++ Data_Validation.py
@@ -51,7 +51,7 @@ try:
     print("Incorrect Data Types in Specific Columns:", incorrect_data_types)
 
     # Check for unique values in specific columns
-    columns_to_check_uniqueness = ['PEOPLE_iD']
+    columns_to_check_uniqueness = ['PEOPLE_ID']
     unique_values_specific_columns = {col_name: df.select(col_name).distinct().count() for col_name in columns_to_check_uniqueness}
     print("Unique Values in Specific Columns:", unique_values_specific_columns)
 
