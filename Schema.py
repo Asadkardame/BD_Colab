a9b19159c1513b37ae2e2bc8379585669d74f5fe
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-18 19:22:56 +0000

    Update Schema.py

--- Schema.py
+++ Schema.py
@@ -20,7 +20,7 @@ hive_table_name = "usukprjdb.people"
 
 postgres_df = spark.read.format("jdbc") \
     .option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb") \
-    .option("dbtable", "people") \
+    .option("dbtable", "people2") \
     .option("user", "consultants") \
     .option("password", "WelcomeItc@2022") \
     .option("driver", "org.postgresql.Driver") \
