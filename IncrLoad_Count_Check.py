454e3232d2030f5c462cc6a17b951183c7bd3783
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-20 11:38:18 +0000

    Update IncrLoad_Count_Check.py

--- IncrLoad_Count_Check.py
+++ IncrLoad_Count_Check.py
@@ -46,6 +46,9 @@ class TestIncrDataLoading(unittest.TestCase):
             print('Postgres_Count', postgres_count)
             print('Hive_count', Hive_count)
         else:
+            print("Number of rows loaded to Hive matches the expected count")
+            print('Postgres_Count', postgres_count)
+            print('Hive_count', Hive_count)
             self.assertEqual(Hive_count, postgres_count, "Number of rows loaded to Hive does not match expected count")
 
 if __name__ == '__main__':
